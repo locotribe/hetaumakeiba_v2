0e0e269ae5808549fcd221a3a344d9c83e28f58f
locotribe <kiichi.co.jp@gmail.com>
2025-07-29 00:08:30 +0900

    v8.6.0 競走馬データの取得 のための URL生成 関数を作成

--- lib/utils/url_generator.dart
+++ lib/utils/url_generator.dart
@@ -11,3 +11,11 @@ String generateNetkeibaUrl({
       .join();
   return 'https://db.netkeiba.com/race/20$suffix';
 }
+
+/// 競走馬のデータベースページURLを生成します。
+/// [horseId] は競走馬のID（例: '2020101779'）です。
+String generateNetkeibaHorseUrl({
+  required String horseId,
+}) {
+  return 'https://db.netkeiba.com/horse/$horseId';
+}
